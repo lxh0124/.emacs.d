e8505200a8a229530a6961413551d4c0e937af31
Tony <lxh0124@126.com>
2018-04-01 07:26:24 +0800

    add Git Tool

--- init.el
+++ init.el
@@ -73,9 +73,10 @@
   :config
   (window-numbering-mode 1))
 
-(use-package blackboard-theme
+(use-package solarized-theme
   :config
-  (load-theme 'blackboard t))
+  (load-theme 'solarized-dark t))
+
 
 (use-package helm
   :diminish helm-mode
@@ -173,16 +174,19 @@
   (projectile-global-mode))
 (use-package helm-projectile)
 
-
+(use-package magit)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(package-selected-packages
    (quote
-    (helm-projectile helm-swoop color-theme-solarized color-theme miniedit smart-mode-line helm-descbinds auto-compile window-numbering use-package projectile js2-refactor helm company blackboard-theme))))
+    (magit solarized-theme helm-projectile helm-swoop color-theme-solarized color-theme miniedit smart-mode-line helm-descbinds auto-compile window-numbering use-package projectile js2-refactor helm company blackboard-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
